99d4bf6e0a0edfedcdb75c1dd78970993481dfab
Truong <tnp152@gmail.com>
2025-08-13 22:09:46 +0700

    feat: Add GroupBookingQR model for handling group booking data and serialization

--- lib/data/models/individual_qr_models.g.dart
+++ lib/data/models/individual_qr_models.g.dart
@@ -88,6 +88,52 @@ Map<String, dynamic> _$TourBookingGuestModelToJson(
       'totalGuests': instance.totalGuests,
     };
 
+GroupBookingQR _$GroupBookingQRFromJson(Map<String, dynamic> json) =>
+    GroupBookingQR(
+      bookingId: json['bookingId'] as String,
+      bookingCode: json['bookingCode'] as String,
+      bookingType: json['bookingType'] as String,
+      groupName: json['groupName'] as String?,
+      groupDescription: json['groupDescription'] as String?,
+      numberOfGuests: (json['numberOfGuests'] as num).toInt(),
+      tourOperationId: json['tourOperationId'] as String?,
+      tourSlotId: json['tourSlotId'] as String?,
+      tourTitle: json['tourTitle'] as String?,
+      tourDate: json['tourDate'] as String,
+      contactName: json['contactName'] as String?,
+      contactEmail: json['contactEmail'] as String?,
+      contactPhone: json['contactPhone'] as String?,
+      totalPrice: (json['totalPrice'] as num).toDouble(),
+      originalPrice: (json['originalPrice'] as num).toDouble(),
+      discountPercent: (json['discountPercent'] as num).toDouble(),
+      generatedAt: json['generatedAt'] as String,
+      qrType: json['qrType'] as String,
+      version: json['version'] as String,
+    );
+
+Map<String, dynamic> _$GroupBookingQRToJson(GroupBookingQR instance) =>
+    <String, dynamic>{
+      'bookingId': instance.bookingId,
+      'bookingCode': instance.bookingCode,
+      'bookingType': instance.bookingType,
+      'groupName': instance.groupName,
+      'groupDescription': instance.groupDescription,
+      'numberOfGuests': instance.numberOfGuests,
+      'tourOperationId': instance.tourOperationId,
+      'tourSlotId': instance.tourSlotId,
+      'tourTitle': instance.tourTitle,
+      'tourDate': instance.tourDate,
+      'contactName': instance.contactName,
+      'contactEmail': instance.contactEmail,
+      'contactPhone': instance.contactPhone,
+      'totalPrice': instance.totalPrice,
+      'originalPrice': instance.originalPrice,
+      'discountPercent': instance.discountPercent,
+      'generatedAt': instance.generatedAt,
+      'qrType': instance.qrType,
+      'version': instance.version,
+    };
+
 IndividualGuestCheckInRequest _$IndividualGuestCheckInRequestFromJson(
         Map<String, dynamic> json) =>
     IndividualGuestCheckInRequest(
